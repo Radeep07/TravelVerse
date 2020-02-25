fec7cba8e1cc5b5d5be756413168006f1ff2172a
David Brown <david.bn@bigpond.com>
2020-02-25 21:45:26 +1030

    implemented login and signup routes

---
+++ schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS travelVerse_db;
+CREATE database travel;
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require("express");
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
 
 var PORT = process.env.PORT || 3000;
 
@@ -13,6 +16,12 @@ app.use(express.static("public"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
